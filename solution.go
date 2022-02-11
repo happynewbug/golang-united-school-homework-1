19f3ca49afc25bdf9a2867ccf6699061c9ed8b20
Eldar Azimov <chaevnicher@gmail.com>
2022-02-11 21:45:28 +0300

    HW-001: delete main func

--- solution.go
+++ solution.go
@@ -1,14 +1,9 @@
 package solution
 
 import (
-	"fmt"
 	"github.com/kyokomi/emoji"
 )
 
 func GetMessage() string {
 	return emoji.Sprint("Hello 🗺️!")
-}
-
-func main() {
-	fmt.Println(GetMessage())
 }
