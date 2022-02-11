7b976d5f7e721628c39a4dd4b6317a1132b8c8d1
Eldar Azimov <chaevnicher@gmail.com>
2022-02-11 22:33:11 +0300

    HW-001: change emo to template and fix test

--- solution_test.go
+++ solution_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestGetMessage(t *testing.T) {
+	expected := string([]byte{72, 101, 108, 108, 111, 32, 240, 159, 151, 186, 239, 184, 143, 32, 33})
 	actual := GetMessage()
-	expect := "Hello 🗺️!"
 
-	if !strings.EqualFold(actual, expect) {
-		t.Errorf("actual %q expect %q", actual, expect)
+	if !strings.EqualFold(actual, expected) {
+		t.Errorf("actual %q expect %q", actual, expected)
 	}
 
 	t.Log("OK!")
